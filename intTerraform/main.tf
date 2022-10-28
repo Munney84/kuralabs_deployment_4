e6d0dd1165097b459ddd8437a1fa9d41c379b72d
Munney84 <108026310+Munney84@users.noreply.github.com>
2022-10-28 18:33:31 -0400

    updated AMI

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server01" {
-  ami = "ami-097a2df4ac947655f"
+  ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "awsKey"
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
