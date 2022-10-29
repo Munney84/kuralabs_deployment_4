64fbd9fbcdfb6adbc06f38e77e654c226ed07d43
Munney84 <108026310+Munney84@users.noreply.github.com>
2022-10-29 09:05:00 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -1,11 +1,16 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "vpc_id" {}
 
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region = "us-east-1"
   
+}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
 }
 
 resource "aws_instance" "web_server01" {
